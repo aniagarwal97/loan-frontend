4102a210b482bb8483baff2ec588443bd6b5556b
geetkalaniexternlabs <geetkalani@exeternlabs.com>
2019-10-18 00:40:46 +0530

    add the analysis column with analysis feature

--- src/Containers/Upload/index.jsx
+++ src/Containers/Upload/index.jsx
@@ -23,21 +23,28 @@ class Upload extends Component {
                 <div className='upload_files_head'>
                     <div className='upload_doc'>UPLOADED DOCUMENTS</div>
                     <div className='upload_time'>CREATION DATE</div>
+                    <div className='analysis'>ANALYSIS</div>
                 </div>
                 <div className='uploaded_files clickable-item' onClick = {this.handleDocumentClick}>
                     <div className='extension'>BOX</div>
                     <div className='uploaded_doc'>DOC NAME</div>
                     <div className='uploaded_time'>12/12/12</div>
+                    <div className='analysis_img'><img src="../../Assets/analysis.gif" /></div>
+
                 </div>
                 <div className='uploaded_files clickable-item'>
                     <div className='extension'>BOX</div>
                     <div className='uploaded_doc'>DOC NAME</div>
                     <div className='uploaded_time'>13/12/11</div>
+                    <div className='analysis_doc'><span className='fa fa-check' style={{color: 'green'}}></span></div>
+
                 </div>
                 <div className='uploaded_files clickable-item'>
                     <div className='extension'>BOX</div>
                     <div className='uploaded_doc'>DOC NAME</div>
                     <div className='uploaded_time'>08/09/10</div>
+                    <div className='analysis_doc'><span className='fa fa-check' style={{color: 'green'}}></span></div>
+
                 </div>
                    
             </div>
