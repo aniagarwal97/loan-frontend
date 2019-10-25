e17092a3312f296177ef6963d8733809bdde033e
anuj jain <anujjain@externlabs.com>
2019-10-25 23:18:33 +0530

    user approval page

---
+++ src/Containers/DashboardUpload/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router';
+import './style.scss'
+
+export default class DashboardUpload extends Component {
+    render() {
+        return (
+            <div>
+                hey this is DashboardUpload page
+            </div>
+        )
+    }
+}
