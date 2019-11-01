ef9f65d2b58a4e5659f15ff98028aacfd1794ba8
Aniruddh Agarwal <aniagarwal97@gmail.com>
2019-11-01 10:25:30 +0530

    Added fetch all inactive users API

--- src/Containers/UserApproval/index.jsx
+++ src/Containers/UserApproval/index.jsx
@@ -1,38 +1,56 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import './style.scss'
+import { fetchInactiveUsers } from '../../Actions/UserApproval';
 
-export default class UserApproval extends Component {
+class UserApproval extends Component {
+
+    componentDidMount(){
+        this.props.fetchInactiveUsers()
+    }
+
+    handleAcceptUser = (user) => {
+        
+    }
     render() {
         return (
             <div className="user_approval_page">
-                <div className="user_approval">
-                    <div className="username">NAME</div>
-                    <div className="organization">ORGANIZATION</div>
-                    <div className="request">REQUEST</div>
-                </div>
-                <div className="list_user">
-                    <div className="user_name">Aniruddh</div>
-                    <div className="org">ExternLabs</div>
-                    <div className="req"><span className="accept">Accept</span> / <span className="reject">Reject</span></div>
-                </div>
-                <div className="list_user">
-                    <div className="user_name">Zach</div>
-                    <div className="org">ABC</div>
-                    <div className="req"><span className="accept">Accept</span> / <span className="reject">Reject</span></div>
-                </div>
-                <div className="list_user">
-                    <div className="user_name">Mike</div>
-                    <div className="org">DEF</div>
-                    <div className="req"><span className="accept">Accept</span> / <span className="reject">Reject</span></div>
-                </div>
-                <div className="list_user">
-                    <div className="user_name">Mark</div>
-                    <div className="org">GHI</div>
-                    <div className="req"><span className="accept">Accept</span> / <span className="reject">Reject</span></div>
-                </div>
-
+                <table className='user_approval_table'>
+                    <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Organization</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.inactiveUsers && this.props.inactiveUsers.length && this.props.inactiveUsers.map((value, index) => {
+                                return (
+                                    <tr>
+                                        <td>{value.first_name}</td>
+                                        <td>{value.second_name}</td>
+                                        <td>{value.email}</td>
+                                        <td>{value.org}</td>
+                                        <td className='clickable-item'><span className="accept" onClick = {() => {this.handleAcceptUser(value.email)}}>Accept</span> / <span className="reject">Reject</span></td>
+                                    </tr>)
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
         )
     }
 }
+
+
+const mapStateToProps = state => ({
+    inactiveUsers: state.inactiveUsers
+});
+const mapDispatchToProps = dispatch => ({
+    fetchInactiveUsers: bindActionCreators(fetchInactiveUsers, dispatch),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(UserApproval);
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,10 +2,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './loginReducer';
 import documentReducer from './documentReducer';
+import userApprovalReducer from './userApprovalReducer';
 
 const appReducer = combineReducers({
   user: authReducer,
-  documents: documentReducer
+  documents: documentReducer,
+  inactiveUsers: userApprovalReducer 
 });
 
 const rootReducer = (state, action) => {
