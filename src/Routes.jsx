299197870669d125d0f7509766b299ff536b9947
Aniruddh Agarwal <aniagarwal97@gmail.com>
2019-11-06 22:02:07 +0530

    .

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 // import UserAuthentication from './Containers/UserAuthentication';
 import Layout from './Containers/Layout';
 import Login from './Containers/Login';
@@ -10,6 +10,7 @@ export default class Routes extends Component {
     render() {
         return (
             <div>
+                <BrowserRouter basename="/app/" />
                 <Route path="/register" exact = {true} component={Register} />
                 <Route path="/login" exact = {true} component={Login} />
                 <Route path = "/layout" component = {Layout} />
