185cf077bdb4e68b1248b95991bf721851d05921
Aniruddh Agarwal <aniagarwal97@gmail.com>
2019-11-08 17:07:18 +0530

    logo update

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter,Link } from 'react-router-dom';
 import './styles.scss';
+import logo from '../../Assets/logo_ZT.svg';
 
 class Header extends Component {
     
@@ -16,7 +17,7 @@ class Header extends Component {
         const isUserAdmin = localStorage.getItem('is_admin')
         return (
             <div className='header'>
-                <div className='logo clickable-item'><img onClick={()=>{this.props.history.push('/app/login')}} src={require('../../Assets/logo_ZT.svg')} alt='logo' width='180' height='100' /></div>
+                <div className='logo clickable-item'><img onClick={()=>{this.props.history.push('/app/login')}} src={logo} alt='logo' width='180' height='100' /></div>
                 {isUserAdmin && <div className='opt1'><span><Link style={{color:'black', textDecoration:'none'}} to="/app/layout/user-approval">User Approval</Link></span></div>}
                 {isUserAdmin && <div className='opt2'><span><Link style={{color:'black', textDecoration:'none'}} to="/app/layout/document-list">Document List</Link></span></div>}
                 {/* <div className='opt3'><span><Link style={{color:'black', textDecoration:'none'}} to="/app/layout/user-list">User List</Link></span></div> */}
